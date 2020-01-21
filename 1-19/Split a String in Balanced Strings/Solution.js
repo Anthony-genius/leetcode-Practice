d2bd254c47dd14fa4447592725cc749a7ce80832
unknown <loyalsun94415@hotmail.com>
2020-01-21 08:36:30 +0800

    Solving two problems

--- 1-19/Split a String in Balanced Strings/Solution.js
+++ 1-19/Split a String in Balanced Strings/Solution.js
@@ -1,21 +1,14 @@
-public class Solution {
-    public int BalancedStringSplit(string s) {
-        int lCount = 0;
-        int rCount = 0;
-        int balancedSubstringCount = 0;
-
-        foreach (Char ch in s) {
-            if (ch == 'R') {
-                ++rCount;
-            } else {
-                ++lCount;
-            }
-
-            if (lCount == rCount) {
-                ++balancedSubstringCount;
-            }
-        }
-
-        return balancedSubstringCount;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let lcount = 0, rcount = 0, count=0;
+    for(let c of s){
+        if(c === "R") rcount++;
+        else if(c==="L") lcount++;
+        if(lcount==rcount) count++
+        
     }
-}
+    return count;
+};
---
+++ commit.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Solving two problems"
+git push -u origin master
